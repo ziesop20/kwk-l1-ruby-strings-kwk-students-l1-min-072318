83351837d249f75a6ac8c4d3608790f44b8df5ec
ziesop20 <sincerelysophie@hotmail.com>
2018-07-23 20:52:46 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -8,4 +8,4 @@ puts "What time is the party?"
 time = gets.chomp 
 puts "who is hosting the party?"
 host_name = gets.chomp 
-puts "Dear #{guest_name}, You are cordially invited to #{party_name} on #{date} at #{time}. It will be a great night full of gourmet food, dancing to a world class DJ, and many dogs. I can't wait to see you there. Please RSVP within two weeks of recieving this letter. Most sincerely, #{host_name}"
+puts "Dear #{guest_name}, You are cordially invited to #{party_name} on #{date} from #{time}. It will be a great night full of gourmet food, dancing to a world class DJ, and many dogs. I can't wait to see you there. Please RSVP within two weeks of recieving this letter. Most sincerely, #{host_name}"
